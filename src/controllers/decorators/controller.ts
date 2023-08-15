bd58ab9d732b129814b24447d6b1ccc372050114
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-08-15 22:55:40 +0530

    added use decorater to add middlewares

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
 import { Methods } from "./Methods";
+import { Metadatakeys } from "./MetadataKeys";
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
@@ -9,18 +10,26 @@ export function controller(routePrefix: string) {
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
 
-      const path = Reflect.getMetadata("path", target.prototype, key);
-
-      //console.log(`${routePrefix}${path}`, routeHandler);
-
-      const method: Methods = Reflect.getMetadata(
-        "method",
+      const path = Reflect.getMetadata(
+        Metadatakeys.path,
         target.prototype,
         key
       );
 
+      //console.log(`${routePrefix}${path}`, routeHandler);
+
+      const method: Methods = Reflect.getMetadata(
+        Metadatakeys.method,
+        target.prototype,
+        key
+      );
+
+      const middlewares =
+        Reflect.getMetadata(Metadatakeys.middleware, target.prototype, key) ||
+        [];
+
       if (path) {
-        router[method](`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
       } else {
         console.log("error occured");
       }
