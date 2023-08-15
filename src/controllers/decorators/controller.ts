7e59a2f2c6b7588815e56c5e03ef6f9a0721d2c9
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-08-15 18:41:04 +0530

    first decorater created controller working

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,10 +1,21 @@
 import "reflect-metadata";
+import express from "express";
+
+export const router = express.Router();
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
+
       const path = Reflect.getMetadata("path", target.prototype, key);
+      console.log(`${routePrefix}${path}`, routeHandler);
+
+      if (path) {
+        router.get(`${routePrefix}${path}`, routeHandler);
+      } else {
+        console.log("error occured");
+      }
     }
   };
 }
