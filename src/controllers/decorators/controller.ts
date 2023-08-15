58e3ef560540752cb6df5446288af7b097d641c7
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-08-15 22:12:55 +0530

    added method enums

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
+import { Methods } from "./Methods";
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
@@ -9,10 +10,17 @@ export function controller(routePrefix: string) {
       const routeHandler = target.prototype[key];
 
       const path = Reflect.getMetadata("path", target.prototype, key);
-      console.log(`${routePrefix}${path}`, routeHandler);
+
+      //console.log(`${routePrefix}${path}`, routeHandler);
+
+      const method: Methods = Reflect.getMetadata(
+        "method",
+        target.prototype,
+        key
+      );
 
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, routeHandler);
       } else {
         console.log("error occured");
       }
